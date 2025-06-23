f115d0573be96b5f6c284f01b6aaa9273a70044b
l-luz <lyraluz@yahoo.com.br>
2025-06-23 10:24:45 -0300

    renames e refatoracao

--- lib/models/services/dashboard_service.dart
+++ lib/models/services/dashboard_service.dart
@@ -1,8 +1,8 @@
 // libmodels/ervices/estatistica_service.dart
-import 'package:expenses_control/models/statistics/dashboard_dto.dart';
-import 'package:expenses_control/models/statistics/i_estrategia_dashboard.dart';
-import 'package:expenses_control/models/statistics/relatorio_comum.dart';
-import 'package:expenses_control/models/statistics/relatorio_avancado.dart';
+import 'package:expenses_control/models/dashboard/dashboard_dto.dart';
+import 'package:expenses_control/models/dashboard/i_estrategia_dashboard.dart';
+import 'package:expenses_control/models/dashboard/relatorio_comum.dart';
+import 'package:expenses_control/models/dashboard/relatorio_avancado.dart';
 import 'package:expenses_control/models/gasto.dart';
 
 class DashboardService {
@@ -13,9 +13,9 @@ class DashboardService {
   void setEstrategiaAvancada(IEstrategiaDashboard estrategiaAvancada) => _estrategiaAvancada = estrategiaAvancada;
     
   Future<DashboardDTO> geraDashboardCompleto(List<Gasto> gastos) async {
-    final dtoComum = _estrategiaComum.gerarEstatistica(gastos);
+    final dtoComum = _estrategiaComum.geraRelatorio(gastos);
 
-    final dtoAvancado = _estrategiaAvancada.gerarEstatistica(gastos);
+    final dtoAvancado = _estrategiaAvancada.geraRelatorio(gastos);
 
     // Mescla os resultados usando o método copyWith.
     final dtoFinal = dtoComum.copyWith(
