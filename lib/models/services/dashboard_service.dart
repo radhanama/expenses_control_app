77b8c1c37013609f5b078193a74866c25aceb667
radhanama <radhanama@hotmail.com>
2025-06-24 00:05:06 +0000

    fix

--- lib/models/services/dashboard_service.dart
+++ lib/models/services/dashboard_service.dart
@@ -1,17 +1,19 @@
 // lib/models/services/dashboard_service.dart
-import 'package:expenses_control/models/statistics/dashboard_dto.dart';
-import 'package:expenses_control/models/statistics/i_estrategia_dashboard.dart';
-import 'package:expenses_control/models/statistics/relatorio_comum.dart';
-import 'package:expenses_control/models/statistics/relatorio_avancado.dart';
+import 'package:expenses_control/models/dashboard/dashboard_dto.dart';
+import 'package:expenses_control/models/dashboard/i_estrategia_dashboard.dart';
+import 'package:expenses_control/models/dashboard/relatorio_avancado.dart';
+import 'package:expenses_control/models/dashboard/relatorio_comum.dart';
 import 'package:expenses_control/models/gasto.dart';
 
 class DashboardService {
   IEstrategiaDashboard _estrategiaComum = RelatorioComum();
   IEstrategiaDashboard _estrategiaAvancada = RelatorioAvancado();
 
-  void setEstrategiaComum(IEstrategiaDashboard estrategiaComum) => _estrategiaComum = estrategiaComum;
-  void setEstrategiaAvancada(IEstrategiaDashboard estrategiaAvancada) => _estrategiaAvancada = estrategiaAvancada;
-    
+  void setEstrategiaComum(IEstrategiaDashboard estrategiaComum) =>
+      _estrategiaComum = estrategiaComum;
+  void setEstrategiaAvancada(IEstrategiaDashboard estrategiaAvancada) =>
+      _estrategiaAvancada = estrategiaAvancada;
+
   Future<DashboardDTO> geraDashboardCompleto(List<Gasto> gastos) async {
     final dtoComum = _estrategiaComum.geraRelatorio(gastos);
 
