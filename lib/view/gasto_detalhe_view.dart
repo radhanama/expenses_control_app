40992ec6c00f8a99aeb2b0fa0f2c2c1c09015590
Radhanama <radhanama@hotmail.com>
2025-07-12 13:23:35 -0300

    fix build issues

--- lib/view/gasto_detalhe_view.dart
+++ lib/view/gasto_detalhe_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
-import '../models/gasto.dart';
+import 'package:expenses_control/models/gasto.dart';
 import '../view_model/categoria_view_model.dart';
 import '../view_model/gasto_view_model.dart';
 import '../view_model/extrato_view_model.dart';
